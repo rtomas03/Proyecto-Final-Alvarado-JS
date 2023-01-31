decb98e0b69d0a06b677f25ed46b7e96b9401851
Tomas Alvarado <rtomas03@gmail.com>
2023-01-30 21:57:53 -0300

    ultimos retoques

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ toCurrency = document.querySelector(".to select"),
 getButton = document.querySelector("form button");
 
 
-        for (let i = 0; i < dropList.length; i++) { 
-            for(let currency_code in country_list){
+  for (let i = 0; i < dropList.length; i++) { 
+      for(let currency_code in country_list){
 
-                let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "ARS" ? "selected" : "";
-                let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+            let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "ARS" ? "selected" : "";
+             let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
           
                 dropList[i].insertAdjacentHTML("beforeend", optionTag);
             }
